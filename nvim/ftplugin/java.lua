9779a0cfe61c6378942686f76ae3a2d1b2da7a02
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-02-18 12:29:11 -0500

    ftplugin: indent 4 spaces for several languages

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,17 +1,18 @@
- require("jdtls").start_or_attach({
-     cmd = {
-      "jdtls"
-     },
-    capabilities = require("cmp_nvim_lsp").default_capabilities(),
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 
-    settings =
+require("jdtls").start_or_attach({
+  cmd = { "jdtls" },
+  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+
+  settings =
+  {
+    inlayHints =
     {
-      inlayHints =
+      parameterNames =
       {
-        parameterNames =
-        {
-          enabled = "all", -- Would disable, but it doesn't seem to work and I'd like it to, so leaving it on for debugging
-        }
+        enabled = "all", -- Would disable, but it doesn't seem to work and I'd like it to, so leaving it on for debugging
       }
     }
+  }
 })
