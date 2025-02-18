3509917e64dfb6e54d8abdb6c1e06a725ce790d0
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-02-18 17:24:04 -0500

    ftplugin/help: color links blue

--- nvim/ftplugin/help.lua
+++ nvim/ftplugin/help.lua
@@ -1,2 +1,6 @@
+-- Follow and return from tag link
 noremap("<CR>", "<C-]>")
 noremap("<BS>", "<C-T>")
+
+-- Color tag links blue to be more visually distinct from mini.cursorword
+vim.api.nvim_set_hl(0, "@markup.link.vimdoc", { fg = colors.blue })
