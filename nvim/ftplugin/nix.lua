911d0b1c4d4973966007124f412a92a2baf8c35f
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-02-18 13:38:25 -0500

    indent: use cindent
    cindent is the modern version of smartindent, which I was already
    liking. We do have to disable one issue I was getting with both
    smartindent AND cindent, where in Nix, they would move comments to the
    beginning of the line. We do this in ftplugin for nix.

---
+++ nvim/ftplugin/nix.lua
@@ -0,0 +1,4 @@
+-- We use cindent, which autoformats comments. We don't like that, and force it to stop.
+o.cinkeys:remove("0#")
+
+o.commentstring = '# %s'
--- nvim/lua/myconfig/options.lua
+++ nvim/lua/myconfig/options.lua
@@ -14,13 +14,13 @@ o.ignorecase = true
 o.smartcase = true
 
 vim.cmd('filetype plugin indent on')
-
 o.autoindent = true
-o.smartindent = true
+o.cindent = true -- Smarter than smartindent
+o.smartindent = false
 
 o.expandtab = true -- spaces as tab
 o.tabstop = 2 -- 2 spaces for tabs
-o.shiftwidth = 2 -- 2 spaces for tabs
+o.shiftwidth = 0 -- Reuse value of tabstop
 
 -- Timeout isn't intuitive. We want it for `a` in visual mode, but that's a
 -- PR for another day.
