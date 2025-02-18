3e7793aec537f19a2c2156099535de1298b9d811
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-02-18 12:24:49 -0500

    options: add more QOL options
    smartindent is wonderful!

--- nvim/lua/myconfig/options.lua
+++ nvim/lua/myconfig/options.lua
@@ -8,11 +8,16 @@ o.mouse = "" -- Disable mouse
 o.cursorline = true
 o.hlsearch = true -- Highlight search matches
 o.number = true
+o.breakindent = true -- Continue indented wrapped line at same level
 
 o.ignorecase = true
 o.smartcase = true
 
+vim.cmd('filetype plugin indent on')
+
 o.autoindent = true
+o.smartindent = true
+
 o.expandtab = true -- spaces as tab
 o.tabstop = 2 -- 2 spaces for tabs
 o.shiftwidth = 2 -- 2 spaces for tabs
@@ -20,5 +25,10 @@ o.shiftwidth = 2 -- 2 spaces for tabs
 -- Timeout isn't intuitive. We want it for `a` in visual mode, but that's a
 -- PR for another day.
 o.timeout = false
+
+o.smoothscroll = true -- Shows a continuation `>>>` when wrapping line is cut off
+
+o.backup = false
+o.writebackup = false
 
 g.mapleader = " "
