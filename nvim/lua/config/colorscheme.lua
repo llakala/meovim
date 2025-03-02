7037269bb558c62def858f9f5b8ae34afa3e8ee6
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-02 16:05:34 -0500

    colorscheme: highlight cursorcolumn better

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,5 +1,6 @@
 -- Global variable for access elsewhere
--- Check the value with: `:tabnew Colors|pu=execute(':=colors')`
+-- Check the value with:
+-- :tabnew Colors|pu=execute(':=colors')
 -- Putting it in a buffer lets it be colorized with our hexcode previewer
 -- TODO: add an alias to do this easily
 colors = require("tokyonight.colors").setup()
@@ -12,8 +13,9 @@ require("tokyonight").setup({
   end,
 
 
-  on_highlights = function(highlights, colors)
-     highlights["@variable"] = { fg = colors.red }
+  on_highlights = function(hl, colors)
+     hl["@variable"] = { fg = colors.red }
+     hl.ColorColumn = { bg = colors.bg_highlight }
   end
 })
 
