8f7ffe900a8d1f67dc523c7ee564bd9bc0027c1b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-01 20:57:53 -0500

    wip
    Not sure if this'll work, let's try it

--- nvim/lua/plugins/session.lua
+++ nvim/lua/plugins/session.lua
@@ -1,1 +1,11 @@
-require("auto-session").setup()
+require("auto-session").setup({
+  -- Referenced from https://github.com/rmagatti/auto-session/issues/259#issuecomment-1812949343
+  -- Helps with a corrupt session
+  pre_save_cmds = { "NvimTreeClose" },
+	save_extra_cmds = {
+		"NvimTreeOpen"
+	},
+	post_restore_cmds = {
+		"NvimTreeOpen"
+	}
+})
