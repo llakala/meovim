52cf3558e0c5788799ffcd6d5d9f50f9ef7afda6
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-04 20:01:40 -0500

    lsp/lua: add `ya` and `cx` to globals
    For yazi dev!

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -31,7 +31,7 @@ require("lspconfig").lua_ls.setup({
     {
       diagnostics =
       {
-        globals = { "vim", "require" },
+        globals = { "vim", "require", "ya", "cx" },
         -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
         -- https://github.com/nvim-lua/kickstart.nvim/issues/543#issuecomment-1859319206
         disable = { "missing-fields" },
