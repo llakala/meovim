a2a964f915984a8fdd8d935a15acbc272719af13
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-08 13:42:39 -0500

    plugins/pairs: disable auto parentheses in gleam
    This was giving a false positive, where if I did:
    ```
    3 |> io.deb
    ```
    and pressed Tab to complete, it would add parentheses, when it didn't
    need to, since we were in a pipe.
    
    Hopefully this won't make auto pairs start not working in other
    languages, since I technically didn't provide the `*` case. I assume
    it's smart enough to know what I want.

--- nvim/lua/plugins/pairs.lua
+++ nvim/lua/plugins/pairs.lua
@@ -30,5 +30,10 @@ local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
 cmp.event:on(
   'confirm_done',
-  cmp_autopairs.on_confirm_done()
+  cmp_autopairs.on_confirm_done({
+    filetypes =
+    {
+      gleam = false -- Adds parentheses in pipes when we don't want them
+    }
+  })
 )
