5cbecec4e1cbe044da0953c5d7372f0ed804ca40
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-18 19:36:07 -0400

    lsp: add <leader>f to preview docs
    Might change the keybind eventually, but better than not having it.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,6 +1,7 @@
 nnoremap("<leader>r", vim.lsp.buf.rename)
 nnoremap("<leader>a", vim.lsp.buf.code_action)
-nnoremap("<leader>d", vim.diagnostic.open_float)
+nnoremap("<leader>d", vim.diagnostic.open_float) -- d for diagnostics
+nnoremap("<leader>f", vim.lsp.buf.hover)         -- f for function
 
 local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
