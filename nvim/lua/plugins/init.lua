5dfb8d909654add122885f13b847910565882e5b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-18 19:35:32 -0400

    plugins/init.lua: reorganize further
    I need my `x` and my write on format.

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -4,15 +4,17 @@ require("plugins.yazi")
 require("plugins.session")
 require("plugins.bufferline")
 
+-- Less vital, but make my life better.
+require("plugins.conform")
+require("plugins.textobject")
+
 require("plugins.lsp")
 require("plugins.cmp")
 
 require("plugins.mini")
 require("plugins.plugins")
-require("plugins.textobject")
 require("plugins.wilder")
 require("plugins.pairs")
 require("plugins.lualine")
 require("plugins.snacks")
 require("plugins.comment")
-require("plugins.conform")
