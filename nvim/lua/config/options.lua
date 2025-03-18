4fa8c6384e17247e192eacd05d534a07ddf99d7f
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-18 19:35:17 -0400

    options: format

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,20 +4,20 @@ g = vim.g -- Global variables
 o.termguicolors = true
 
 o.clipboard = "unnamed"
-o.undofile = true -- Persistent undo
-o.mouse = "a" -- Enable mouse. I try not to use it, but it's useful for scrolling diagnostics
-o.hidden = false -- Some people swear by it, but I want closing tabs to actually close them
+o.undofile = true   -- Persistent undo
+o.mouse = "a"       -- Enable mouse. I try not to use it, but it's useful for scrolling diagnostics
+o.hidden = false    -- Some people swear by it, but I want closing tabs to actually close them
 o.shiftround = true -- Round to the nearest indentation level when using `<` and `>`
 
 o.cursorline = true
-o.hlsearch = true -- Highlight search matches
+o.hlsearch = true    -- Highlight search matches
 o.number = true
 o.breakindent = true -- Continue indented wrapped line at same level
 o.signcolumn = "yes"
 
 o.showmode = false -- Using lualine
-o.showcmd = false -- Shows when we press keypresses, which we don't need
-o.laststatus = 3 -- Only one statusline, for better separator between horizontal splits
+o.showcmd = false  -- Shows when we press keypresses, which we don't need
+o.laststatus = 3   -- Only one statusline, for better separator between horizontal splits
 
 o.ignorecase = true
 o.smartcase = true
@@ -32,13 +32,16 @@ o.formatoptions = "tcqjro/"
 
 g.loaded_matchit = 1 -- Lets us remap `%`
 
-o.expandtab = true -- spaces as tab
-o.tabstop = 2 -- 2 spaces for tabs
-o.shiftwidth = 0 -- Reuse value of tabstop
+o.expandtab = true   -- spaces as tab
+o.tabstop = 2        -- 2 spaces for tabs
+o.shiftwidth = 0     -- Reuse value of tabstop
 
 -- Timeout isn't intuitive. We want it for `a` in visual mode, but that's a
 -- PR for another day.
 o.timeout = false
+
+-- Open new buffers in new tabs. Helpful for quickfix stuff
+o.switchbuf = "useopen,usetab,newtab"
 
 o.smoothscroll = true -- Shows a continuation `>>>` when wrapping line is cut off
 
