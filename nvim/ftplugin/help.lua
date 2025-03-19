a6d3a436590efa0692286a34d4b662817e5f5924
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-19 17:25:29 -0400

    ftplugin/help: don't wrap and disable indentscope
    For a better help-reading experience.

--- nvim/ftplugin/help.lua
+++ nvim/ftplugin/help.lua
@@ -4,3 +4,7 @@ noremap("<BS>", "<C-T>")
 
 -- Color tag links blue to be more visually distinct from mini.cursorword
 vim.api.nvim_set_hl(0, "@markup.link.vimdoc", { fg = colors.blue })
+
+local o = vim.opt_local
+o.wrap = false
+vim.b.miniindentscope_disable = true
