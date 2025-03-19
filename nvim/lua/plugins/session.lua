7eacf090fb7d7be2def897b1906a1d8e4ed6a3c2
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-18 20:34:49 -0400

    session: save everything but buffers
    I ran `:buffers` recently and learned I had like 60 old buffers open.
    We grab the recommended sessionoptions from the autosession repo, just
    without `buffers`.

--- nvim/lua/plugins/session.lua
+++ nvim/lua/plugins/session.lua
@@ -1,1 +1,4 @@
 require("auto-session").setup()
+
+-- We save most things to session, but *not* buffers.
+vim.o.sessionoptions = "blank,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
