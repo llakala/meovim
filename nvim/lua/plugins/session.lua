b841b2a985746d1b317cac8e089714baaaf1a421
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-19 10:38:06 -0400

    session: only create one session for a git repo
    Some wonderful UX here! Sometimes, I'll cd into a subdirectory of a git
    repo. auto-session ends up creating a new session for that subdir, which
    typically isn't what I want. To get around this, we only *create* a
    session if we're at the root of a git repo. If we're in a git repo, but
    not at the root, we'll reuse the session from the root. This means I can
    be cd'ed anywhere in the repo, and still get the same session!

--- nvim/lua/plugins/session.lua
+++ nvim/lua/plugins/session.lua
@@ -1,4 +1,44 @@
-require("auto-session").setup()
+local session = require("auto-session")
+
+-- Credit for git repo functions goes to:
+-- https://github.com/mrcndz/dotfiles/blob/e1e24413961ee30db12d3a4fed88774d9b6f7406/nvim/lua/utils.lua
+function in_git_repo()
+  local cmd = "git rev-parse --is-inside-work-tree"
+  return vim.fn.system(cmd) == "true\n"
+end
+
+function get_repo_root()
+  local cmd = "git rev-parse --show-toplevel"
+  local output = vim.fn.system(cmd)
+  return vim.fn.trim(output)
+end
+
+function at_repo_root()
+  return get_repo_root() == vim.uv.cwd()
+end
+
+session.setup({
+  -- Only create a new session if you're at the root of a git repo
+  auto_create = at_repo_root,
+
+  -- If you're in a subdirectory of a git repo, activate the session for the repo's root
+  no_restore_cmds = {
+    function()
+      if in_git_repo() and not at_repo_root() then
+        -- Neovim cd, not shell cd. Means when we exit Neovim,
+        -- auto-session will consider us to be in the right
+        -- directory.
+        vim.api.nvim_cmd({
+          cmd = "cd", args = { get_repo_root() }
+        }, {})
+
+        vim.api.nvim_cmd({
+          cmd = "SessionRestore", args = { get_repo_root() }
+        }, {})
+      end
+    end
+  }
+})
 
 -- We save most things to session, but *not* buffers.
 vim.o.sessionoptions = "blank,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
