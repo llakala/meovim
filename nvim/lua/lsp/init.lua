a155ec9628b0e05986349355f9eb4e8d23fabaa3
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-20 13:10:26 -0400

    lsp: set up inlay hints
    Not totally sure on this, but it's functionality - might disable it
    per-language.

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -2,6 +2,8 @@
 lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 lspconfig = require("lspconfig")
 
+vim.lsp.inlay_hint.enable(true)
+
 require("lsp.keybinds")
 
 require("lsp.lua")
