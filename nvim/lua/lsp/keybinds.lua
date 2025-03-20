58e770280254bda5806fd3f9f84ae0d208db673d
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-20 12:17:15 -0400

    lsp/keybinds: use snacks
    Bummer to get rid of the old code as it's not implementation-specific
    and may be more helpful to others as a reference, but I haven't set up
    an `old` repo yet.

--- nvim/lua/lsp/keybinds.lua
+++ nvim/lua/lsp/keybinds.lua
@@ -17,62 +17,52 @@ noremap("<leader>d",
     vim.diagnostic.open_float() -- d for diagnostics
   end)
 
--- I use mnw for Neovim, which provies a wonderful feature that lets
--- me hot-reload the config, while keeping it declarative as a Nix
--- package. However, doing this means that you get duplicate code
--- stored in the `.direnv` folder, which lsps can find and assume
--- is actual code. We filter out anything in `.direnv` or `/nix/store`,
--- as no actual code should be stored there.
-local function filter(items)
-  if type(items) ~= "table" then
-    return items
-  end
 
-  local filtered = {}
+local cfg = {
+  -- Autoconfirm would be nice, but it makes it open in the current
+  -- buffer, which I don't want. If only snacks followed switchbuf!
+  auto_confirm = false,
 
-  for _, value in pairs(items) do
-    -- `definition` uses `targetUri`, `references` uses `uri`
-    -- Lua does some truthy magic here and assigns the right one
-    local path = value.user_data.targetUri or value.user_data.uri
+  -- I use mnw for Neovim, which provies a wonderful feature that lets
+  -- me hot-reload the config, while keeping it declarative as a Nix
+  -- package. However, doing this means that you get duplicate code
+  -- stored in the `.direnv` folder, which lsps can find and assume
+  -- is actual code. We filter out anything in `.direnv` or `/nix/store`,
+  -- as no actual code should be stored there.
+  filter = {
+    paths = {
+      ["/nix/store"] = false,
+      [Snacks.git.get_root() .. "/.direnv"] = false,
+    }
+  },
 
-    if
-        string.match(path, ".direnv") == nil and
-        string.match(path, "/nix/store") == nil
-    then
-      table.insert(filtered, value)
-    end
-  end
-
-  return filtered
-end
-
-
-local function handle_list(options)
-  local items = options.items
-  if #items > 1 then
-    items = filter(items)
-  end
-
-  vim.fn.setqflist({}, ' ', { title = options.title, items = items, context = options.context })
-  if #items == 0 then
-    vim.cmd("echo 'no results'")
-  elseif #items > 1 then
-    vim.cmd("copen")
-  else
-    vim.cmd("cfirst")
-  end
-end
-
+  -- Open selected files in new tab, set in both `list` and `inputs`
+  -- so whether we type for the file or scroll to it, it'll still
+  -- behave the same. snacks is smart and doesn't use the new tab
+  -- if the instance we selected was in the same file.
+  win = {
+    list = {
+      keys = {
+        ["<CR>"] = { "tab", mode = { "n", "i" } },
+      },
+    },
+    input = {
+      keys = {
+        ["<CR>"] = { "tab", mode = { "n", "i" } },
+      },
+    },
+  },
+}
 -- i for implementation
 nnoremap("<leader>i",
   function()
-    vim.lsp.buf.definition({ on_list = handle_list })
+    Snacks.picker.lsp_definitions(cfg)
   end
 )
 
 -- u for usage
 nnoremap("<leader>u",
   function()
-    vim.lsp.buf.references(nil, { on_list = handle_list })
+    Snacks.picker.lsp_references(cfg)
   end
 )
