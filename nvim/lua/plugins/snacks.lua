803692b8e5029e56ec4c98d79649d47edc933e21
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-03-19 21:43:47 -0400

    snacks: use picker for vim.ui.select
    Tired of the default Neovim pickers. I want good ones everywhere!

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,6 +1,14 @@
-require("snacks").setup({
+Snacks = require("snacks")
+
+Snacks.setup({
   quickfile = { enabled = true },
   input = { enabled = true }, -- Doesn't replace all input, but will work for stuff like lsp renames
+  picker = {
+    enabled = true,
+    focus = "list", -- Start in normal mode
+    ui_select = true,
+  }
 })
 
 vim.ui.input = "Snacks.input"
+vim.ui.select = "Snacks.picker.select"
