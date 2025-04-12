b814bcb33b22ad13e247a98abe5641f3cf4e7a99
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-04-12 11:40:56 -0400

    conform: use lsp formatting as fallback
    Forgot about Gleam!

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -8,11 +8,9 @@ require("conform").setup({
     lua = { "stylua" },
   },
 
-  -- I don't currently have any lsp formatters, and often they just seem to randomly
-  -- start being used. So, for now, just don't use them. Better not forget about this
-  -- in the future!
+  -- For gleam
   default_format_opts = {
-    lsp_format = "never",
+    lsp_format = "fallback",
   },
 
   formatters = {
