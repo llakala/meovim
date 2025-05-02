b261205236f6cdf7af4a6b16001b3d16468bf15a
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-05-01 21:04:59 -0400

    plugins: bring back `lspconfig`
    It provides default configs, which are actually really useful! Means we
    don't constantly have to say the `cmd` and the `filetypes`.

--- nvim/lsp/nixd.lua
+++ nvim/lsp/nixd.lua
@@ -5,11 +5,6 @@ return {
     "--semantic-tokens=true", -- NEEDED, makes syntax highlighting much better
   },
 
-  -- Copied in from lspconfig - the default support from `vim.lsp` seems to be
-  -- missing some nixd support
-  filetypes = { "nix" },
-  root_markers = { "flake.nix", "git" },
-
   -- settings.nixd =
   -- {
   --  nixpkgs.expr = "import <nixpkgs> { }",
