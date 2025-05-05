c1497877cbba33fa5c91568fa35cbffed5112595
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-05-05 13:38:14 -0400

    lsp: don't use `cfg`, remove filter logic
    Somehow using a single table seems to have made things not work - and
    somehow the filter logic doesn't seem necessary anymore. No clue why,
    but I'll take it!

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -8,28 +8,6 @@ vim.diagnostic.config({
   },
 })
 
-local cfg = {
-  -- Autoconfirm would be nice, but it makes it open in the current
-  -- buffer, which I don't want. If only snacks followed switchbuf!
-  auto_confirm = false,
-
-  -- I use mnw for Neovim, which provies a wonderful feature that lets
-  -- me hot-reload the config, while keeping it declarative as a Nix
-  -- package. However, doing this means that you get duplicate code
-  -- stored in the `.direnv` folder, which lsps can find and assume
-  -- is actual code. We filter out anything in `.direnv` or `/nix/store`,
-  -- as no actual code should be stored there. The `.direnv` block is
-  -- further down, since sometimes we won't be in a git repo.
-  filter = {
-    paths = {
-      ["/nix/store"] = false,
-    },
-  },
-
-  -- Open files in new tab
-  win = snacks_new_tab,
-}
-
 -- Custom formatting for diagnostics, to only show the diagnostic and the filename
 local format_function = function(item, picker)
   local ret = {}
@@ -47,13 +25,6 @@ local format_function = function(item, picker)
   return ret
 end
 
--- Don't make `.direnv` files searchable. We need to gate this behind a
--- condition to prevent issues where if we're not in a git repo, `get_root`
--- returns null, and lua freaks.
-if Snacks.git.get_root() ~= nil then
-  cfg.filter.paths[Snacks.git.get_root() .. "/.direnv"] = false
-end
-
 nnoremap("<leader>r", vim.lsp.buf.rename)
 noremap("<leader>h", vim.lsp.buf.hover) -- h for help/hover
 
@@ -67,12 +38,18 @@ end)
 
 -- i for implementation
 nnoremap("<leader>i", function()
-  Snacks.picker.lsp_definitions(cfg)
+  Snacks.picker.lsp_definitions({
+    auto_confirm = false,
+    win = snacks_new_tab,
+  })
 end)
 
 -- u for usage
 nnoremap("<leader>u", function()
-  Snacks.picker.lsp_references(cfg)
+  Snacks.picker.lsp_references({
+    auto_confirm = false,
+    win = snacks_new_tab,
+  })
 end)
 
 -- w for workspace. Shows workspace diagnostics, so you can see errors in other
@@ -82,6 +59,7 @@ nnoremap("<leader>w", function()
   Snacks.picker.diagnostics({
     layout = "custom",
     format = format_function,
+
     win = snacks_new_tab,
   })
 end)
