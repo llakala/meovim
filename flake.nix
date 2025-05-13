9878bedfa990b6299f325aed53a7c3e27460a3de
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-05-13 12:44:29 -0400

    structure: rewrite
    Hopefully, this makes things more readable! We get rid of the
    `flake.nix` and `outputs.nix` separation, instead choosing to have a
    single `flake.nix` for handling everything. I like this separation in
    theory, but I want accessibility here.
    
    We also now have all the custom derivations folders in their own folder.
    I'd prefer to name this something like `nix` or `extras`, but I need it
    to come after `nvim` in the alphabet for my life with Yazi cd'ing into
    the folder. I also work on the comments in the flake.nix, so it's
    hopefully clearer.

--- flake.nix
+++ flake.nix
@@ -22,5 +22,128 @@
     };
   };
 
-  outputs = args: import ./outputs.nix args;
+  outputs = { self, nixpkgs, neovimPlugins, ... } @ inputs:
+  let
+    lib = nixpkgs.lib;
+
+    supportedSystems = [ "x86_64-linux" ];
+    forAllSystems = function: lib.genAttrs
+      supportedSystems
+      (system: function nixpkgs.legacyPackages.${system});
+
+    llakaLib = inputs.llakaLib.pureLib; # Don't need any impure functions
+  in
+  {
+    # When I need something and it isn't packaged already somewhere, I package it
+    # myself. I have these accessible as flake inputs, so I can test them in the
+    # REPL. I use my own custom wrapper around
+    # `lib.packagesFromDirectoryRecursive` to automatically get any package stored
+    # in these folders.
+
+
+    # Plugins to be loaded at start. We comment this out for now, because
+    # `packagesFromDirectoryRecursive` will error if the folder is empty.
+    neovimNonLazyPlugins = forAllSystems
+    (
+      pkgs:
+      {}
+
+      # llakaLib.collectDirectoryPackages
+      # {
+      #   inherit pkgs;
+      #   directory = ./other/nonLazyPlugins;
+      # }
+    );
+
+    # Plugins packaged myself that get loaded lazily.
+    neovimLazyPlugins = forAllSystems
+    (
+      pkgs: llakaLib.collectDirectoryPackages
+      {
+        inherit pkgs;
+        directory = ./other/lazyPlugins;
+      }
+    );
+
+    # Custom binaries to add to $PATH
+    neovimPackages = forAllSystems
+    (
+      pkgs: llakaLib.collectDirectoryPackages
+      {
+        inherit pkgs;
+        directory = ./other/packages;
+      }
+    );
+
+    packages = forAllSystems
+    (
+      pkgs: let
+        # Packages and plugins that I grab from elsewhere. Stored in their own
+        # files so I'm not editing monolithic files to add stuff
+        nonLazyPlugins = import ./nonLazyPlugins.nix { inherit pkgs neovimPlugins; };
+        lazyPlugins = import ./lazyPlugins.nix { inherit pkgs neovimPlugins; };
+        packages = import ./packages.nix { inherit pkgs; };
+
+        # Custom derivations that I wrote myself. Need to be turned into a list,
+        # since flake inputs are expected to be attrsets.
+        customNonLazyPlugins = builtins.attrValues self.neovimNonLazyPlugins.${pkgs.system};
+        customLazyPlugins = builtins.attrValues self.neovimLazyPlugins.${pkgs.system};
+        customPackages = builtins.attrValues self.neovimPackages.${pkgs.system};
+      in
+      {
+        default = inputs.mnw.lib.wrap pkgs
+        {
+          appName = "nvim";
+          neovim = pkgs.neovim-unwrapped;
+
+          initLua =
+          /* lua */
+          ''
+            -- Uncomment when you want to profile nvim startup. Be sure to have
+            -- the snacks.nvim repo cloned for this to work!
+
+            -- vim.opt.rtp:append("/home/emanresu/Documents/repos/snacks.nvim/")
+            -- require("snacks.profiler").startup()
+
+            require("config")
+            require("lz.n").load("lazy")
+
+            -- Add to this whenever you add a new server to the `lsp` folder!
+            -- Ridiculous that nvim can't load them for you as far as I can tell
+            vim.lsp.enable({ "fish_lsp", "gleam", "lua_ls", "nixd", "pylsp", "ts_ls" })
+          '';
+
+          plugins.start =
+            nonLazyPlugins
+            ++ customNonLazyPlugins;
+
+          plugins.opt =
+            lazyPlugins
+            ++ customLazyPlugins;
+
+          extraBinPath =
+            packages ++
+            customPackages;
+
+          plugins.dev.config =
+          {
+            pure = ./nvim;
+            impure = "/home/emanresu/Documents/projects/meovim/nvim"; # Absolute path needed
+          };
+        };
+      }
+    );
+
+    devShells = forAllSystems
+    (
+      pkgs:
+      {
+        default = pkgs.mkShellNoCC
+        {
+          packages = lib.singleton self.packages.${pkgs.system}.default.devMode;
+        };
+      }
+    );
+
+  };
 }
