2219ec67ba286f1acc91da22fe4d0c82142d4c7c
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-05-14 13:00:59 -0400

    cutlass: roll custom cut key ourselves
    Now, `q2q` works!

--- nvim/plugin/cutlass.lua
+++ nvim/plugin/cutlass.lua
@@ -1,3 +1,9 @@
 require("cutlass").setup({
-  cut_key = "q", -- Not using macros enough right now to need this
+  -- The default behavior doesn't handle something like `q2q`
+  cut_key = nil,
 })
+
+-- Do this ourselvews, using noremap instead of nnoremap
+-- Means that it works in the operator pending mode, and we can have numbers as
+-- motions
+noremap("q", "d")
