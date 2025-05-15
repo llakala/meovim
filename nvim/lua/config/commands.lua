0b224347bd5d486dc3a47154ad0fff14ef2ec154
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-05-15 18:37:30 -0400

    config/commands: add :Redir
    Nice for sourcing things!

--- nvim/lua/config/commands.lua
+++ nvim/lua/config/commands.lua
@@ -49,3 +49,10 @@ vim.api.nvim_create_user_command("TermHl", function()
   vim.api.nvim_chan_send(chan, table.concat(lines, "\n"))
   vim.api.nvim_win_set_buf(0, b)
 end, { desc = "Highlights ANSI termcodes in curbuf" })
+
+vim.api.nvim_create_user_command("Redir", function(ctx)
+  local lines = vim.split(vim.api.nvim_exec(":" .. ctx.args, true), "\n", { plain = true })
+  vim.cmd("tabnew")
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  vim.opt_local.modified = false
+end, { nargs = "+", complete = "command" })
