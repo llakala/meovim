b06a083316d7d913cb0148559875f89e2e9af2a0
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-05-15 21:18:39 -0400

    commands/redir: add credit, use nvim_exec2
    nvim_exec is deprecated.

--- nvim/lua/config/commands.lua
+++ nvim/lua/config/commands.lua
@@ -50,8 +50,11 @@ vim.api.nvim_create_user_command("TermHl", function()
   vim.api.nvim_win_set_buf(0, b)
 end, { desc = "Highlights ANSI termcodes in curbuf" })
 
+-- Referenced from: https://www.reddit.com/r/neovim/comments/zhweuc/comment/izo9br1
 vim.api.nvim_create_user_command("Redir", function(ctx)
-  local lines = vim.split(vim.api.nvim_exec(":" .. ctx.args, true), "\n", { plain = true })
+  local exec = vim.api.nvim_exec2(":" .. ctx.args, {})
+  local lines = vim.split(exec.output or "", "\n", { plain = true })
+
   vim.cmd("tabnew")
   vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
   vim.opt_local.modified = false
