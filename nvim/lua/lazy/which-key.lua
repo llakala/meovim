8d1074b14e05cccd967d8b41127ce67118b7819d
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-05-24 00:18:06 -0400

    which-key: add options, add `gq` bind
    Don't know how it isn't there by default! Folke seems to go semi-MIA.

--- nvim/lua/lazy/which-key.lua
+++ nvim/lua/lazy/which-key.lua
@@ -1,8 +1,16 @@
 return {
   "which-key.nvim",
   after = function()
-    require("which-key").setup({
+    local wk = require("which-key")
+    wk.setup({
       preset = "modern",
+      delay = 0,
     })
+
+    local mappings = {
+      preset = true,
+      { "gq", desc = "Format" },
+    }
+    wk.add(mappings)
   end,
 }
