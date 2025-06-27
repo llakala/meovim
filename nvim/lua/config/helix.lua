08db1f164368f0ecbca8185919deb05b387cf550
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-06-26 22:15:33 -0400

    config/helix: remove `mi` and `ma` keybinds
    I've moved past this.

--- nvim/lua/config/helix.lua
+++ nvim/lua/config/helix.lua
@@ -1,7 +1,3 @@
 -- i<Esc> won't move the cursor at all, while a<Esc> will move the cursor
 -- one to the right. I prefer this, as I use i more than a.
 inoremap("<Esc>", "<Esc>l")
-
--- match-in-word and match-around-word
-nnoremap("mi", "vi")
-nnoremap("ma", "va")
