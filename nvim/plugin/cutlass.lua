a834b685226b0aa3e7d436e0755ea0939dfe0ed5
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-07-12 23:00:52 -0400

    cutlass: `D` -> `<A-d>`
    Been actually using `D` for deleting until the end of the line, and I
    don't want to always do `d$`.

--- nvim/plugin/cutlass.lua
+++ nvim/plugin/cutlass.lua
@@ -1,9 +1,13 @@
 require("cutlass").setup({
   -- The default behavior doesn't handle something like `q2q`
+  -- I set up the key myself
   cut_key = nil,
 })
 
--- Doing this myself, since I trust myself to set it up correctly
-nnoremap("D", "d", { desc = "Delete and yank" })
-onoremap("D", "d", { desc = "Delete and yank" })
-vnoremap("D", "d", { desc = "Delete and yank" })
+nnoremap("<A-d>", "d", { desc = "Delete and yank" })
+onoremap("<A-d>", "d", { desc = "Delete and yank" })
+vnoremap("<A-d>", "d", { desc = "Delete and yank" })
+
+nnoremap("<A-S-d>", "D", { desc = "Delete until end of line" })
+onoremap("<A-S-d>", "D", { desc = "Delete until end of line" })
+vnoremap("<A-S-d>", "D", { desc = "Delete until end of line" })
