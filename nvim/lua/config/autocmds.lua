763a2d9a09cae86aafe920b178a96db6e3b9a50c
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-07-15 10:59:21 -0400

    config/autocmds: init
    I'd prefer to have one file for autocmds, rather than multiple for each
    feature. We also tweak the `require` order a bit.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+})
+
 -- When unsuspending, check if the file has been modified outside of nvim. If
 -- so, give us a notification to choose what to do. Not a very pretty
 -- notification - I really need a plugin for this. But better than nothing!
