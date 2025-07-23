aeaffe1744932271aab42bde2cbf8e3b6a50b42a
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-07-23 12:32:04 -0400

    telescope: move builtin keybinds to folder
    I don't like having plugin stuff in `config` - it's nice in theory to
    keep all the keybinds together, but now that I know that the `plugin`
    folder can recurse arbitrarily, making a `telescope` folder is
    preferred.

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -7,8 +7,6 @@ vim.diagnostic.config({
   },
 })
 
-local tlscp = require("telescope.builtin")
-
 nnoremap("<leader>r", vim.lsp.buf.rename, { desc = "Rename symbol" })
 
 -- Mode independent - will show code actions on selection if
@@ -19,20 +17,3 @@ vnoremap("<leader>a", vim.lsp.buf.code_action, { desc = "Code action" })
 nnoremap("<leader>d", function()
   vim.diagnostic.open_float() -- d for diagnostics
 end, { desc = "Diagnostic" })
-
--- i for implementation
-nnoremap("<leader>i", function()
-  tlscp.lsp_definitions()
-end, { desc = "View implementation" })
-
--- u for usage
-nnoremap("<leader>u", function()
-  tlscp.lsp_references()
-end, { desc = "View usage(s)" })
-
--- w for workspace. Shows workspace diagnostics, so you can see errors in other
--- files. Great for Gleam dev, since the Gleam LSP gets stuck if one file has errors.
--- Note that this doesn't work for all LSPs!
-nnoremap("<leader>w", function()
-  tlscp.diagnostics()
-end, { desc = "Workspace diagnostics" })
