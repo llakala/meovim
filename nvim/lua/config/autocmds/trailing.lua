2c06aed77c29913cd6639c014322d0718447d411
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-07-24 11:21:09 -0400

    trailing: show on other lines in insert mode

--- nvim/lua/config/autocmds/trailing.lua
+++ nvim/lua/config/autocmds/trailing.lua
@@ -3,6 +3,8 @@ vim.api.nvim_set_hl(0, "TrailingWhitespace", { bg = "#e55c7a" })
 vim.api.nvim_create_autocmd({ "InsertLeave", "BufWinEnter" }, {
   pattern = "*",
   callback = function()
+    -- buftype logic prevents weird windows like completions from getting
+    -- trailing whitespace highlighting.
     if vim.bo.buftype == "" then
       vim.cmd([[match TrailingWhitespace /\s\+$/]])
     end
@@ -11,5 +13,8 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "BufWinEnter" }, {
 
 vim.api.nvim_create_autocmd("InsertEnter", {
   pattern = "*",
-  command = [[call clearmatches()]],
+
+  -- Only shows trailing whitespace on OTHER lines, not the line you're
+  -- currently typing on. Thanks, vimwiki!
+  command = [[match trailingWhitespace /\s\+\%#\@<!$/]],
 })
