029db05816d33ce67ad76cbc5c1987b46440a153
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-07-30 16:27:22 -0400

    yazi: remove ffmpeg and zoxide deps
    I will have a normal-sized closure, even if it kills me!

--- binaries.nix
+++ binaries.nix
@@ -1,5 +1,22 @@
 { pkgs }:
 let
+  # We remove zoxide and ffmpeg as dependencies. zoxide being included means
+  # yazi doesn't use the global zoxide database, while ffmpeg increases the
+  # closure by 500MB, and I'm not using Yazi for video stuff.
+  customYazi = pkgs.yazi.override {
+    optionalDeps = with pkgs; [
+      jq
+      poppler-utils
+      _7zz
+      fd
+      ripgrep
+      fzf
+      imagemagick
+      chafa
+      resvg
+    ];
+  };
+
   packages = with pkgs; [
     # Language servers
     nixd
@@ -21,7 +38,6 @@ let
     texliveBasic
     texlivePackages.latexmk
     texlivePackages.biber
-    yazi
   ];
 
-in packages
+in packages ++ [ customYazi ]
