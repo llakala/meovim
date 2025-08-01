970330818bff2fdfc94ab165866b7454743900b9
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-08-01 01:54:29 -0400

    surround: change bindings
    Swap forward and backwards brackets, and set some `cur` and `line`
    bindings. Now, `s` is for generic surround, and `ss` is for the full
    line, just like `cc` and `dd`. `S` is for if you want the surround to be
    on newlines.
    
    Note that we keep insert mode and `change_line` maps unbound. I never
    do control binds in insert mode, and `change_line` is weird - changing
    something shouldn't mess with whether it's on newlines or not.

--- nvim/plugin/surround.lua
+++ nvim/plugin/surround.lua
@@ -1,11 +1,48 @@
+local left_delete = "^(.)().-(.)()$"
+local right_delete = "^(. ?)().-( ?.)()$"
+
 require("nvim-surround").setup({
   move_cursor = "sticky",
 
+  -- The defaults use ( for whitespace, and ) for no whitespace. Silly!
+  -- See https://github.com/kylechui/nvim-surround/issues/384
+  surrounds = {
+    ["("] = {
+      add = { "(", ")" },
+      delete = left_delete,
+    },
+    [")"] = {
+      add = { "( ", " )" },
+      delete = right_delete,
+    },
+
+    ["["] = {
+      add = { "[", "]" },
+      delete = left_delete,
+    },
+    ["]"] = {
+      add = { "[ ", " ]" },
+      delete = right_delete,
+    },
+
+    ["{"] = {
+      add = { "{", "}" },
+      delete = left_delete,
+    },
+    ["}"] = {
+      add = { "{ ", " }" },
+      delete = right_delete,
+    },
+  },
+
   keymaps = {
+    insert = false,
+    insert_line = false,
+
     normal = "s",
+    normal_cur = "ss",
+    normal_line = "S",
     normal_cur_line = false,
-    normal_cur = false,
-    normal_line = false,
 
     delete = "ds",
 
@@ -13,10 +50,7 @@ require("nvim-surround").setup({
     change_line = false,
 
     visual = "s",
-    visual_line = false,
-
-    insert = false,
-    insert_line = false,
+    visual_line = "S",
   },
 })
 
