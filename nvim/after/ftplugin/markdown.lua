31df1cf601f2dac94209b3abccb527621e05c4e7
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-08-01 18:16:08 -0400

    surround: add lua and markdown-specific surrounds
    For ``` and [[.

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,1 +1,14 @@
 o.wrap = true
+
+require("nvim-surround").buffer_setup({
+  surrounds = {
+    ["c"] = {
+      add = { { "", "```", "" }, { "", "```", "" } },
+      find = "```.-```",
+
+      -- From https://github.com/gen4438/dotfiles/blob/0822a4bc6d652bf3c7d03adc3020808861d448d1/dot_config/nvim/lua/plugins/vim-surround.lua#L57
+      -- Slightly modified to preserve the wrapping lines
+      delete = "^(```.-)()%\n.-(```)()$",
+    },
+  },
+})
