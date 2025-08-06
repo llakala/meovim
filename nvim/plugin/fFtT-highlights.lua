aee763d7f53e2fffc117909fb7602601642b7963
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-08-06 01:25:47 -0400

    fFtT-highlights: show jumpable chars
    Don't love the highlights on this, but might as well start with
    something decent.

--- nvim/plugin/fFtT-highlights.lua
+++ nvim/plugin/fFtT-highlights.lua
@@ -1,10 +1,16 @@
 require("fFtT-highlights"):setup({
   smart_motions = true,
   reset_key = "<Esc>",
+
   match_highlight = {
     -- If I go past a match, still show it, as long as it's in the closest 5.
     -- Means if I accidentally skip the match I wanted, it stays highlighted!
     persist_matches = 5,
+  },
+
+  jumpable_chars = {
+    show_instantly_jumpable = "on_key_press",
+    show_all_jumpable_in_words = "on_key_press",
   },
 
   -- There are two types of resets: `f<Esc>`, where we clear the gray text, and
