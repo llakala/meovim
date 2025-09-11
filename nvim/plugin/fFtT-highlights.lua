5041f46fd24e55b97a2a5f5de7cb4aeaa5b7a2e9
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-09-11 17:14:07 -0400

    fFtT-highlights: reformat comments
    `gwac`!

--- nvim/plugin/fFtT-highlights.lua
+++ nvim/plugin/fFtT-highlights.lua
@@ -26,17 +26,17 @@ require("fFtT-highlights"):setup({
   },
 
   -- There are two types of resets: `f<Esc>`, where we clear the gray text, and
-  -- `fi<Esc>`, where we clear the orange highlights. This ONLY adds `nohlsearch`
-  -- and `echo` to the FIRST kind.
+  -- `fi<Esc>`, where we clear the orange highlights. This only adds
+  -- `nohlsearch` and `echo` to the FIRST kind.
   on_reset = function()
     vim.cmd.nohlsearch()
     vim.cmd.echo()
   end,
 })
 
--- This is the SECOND kind of reset: `fi<Esc>`. You'll notice that this doesn't actually
--- use any mechanisms from the plugin. However, it still clears the orange
--- highlights!
+-- This is the SECOND kind of reset: `fi<Esc>`. You'll notice that this doesn't
+-- actually use any mechanisms from the plugin. However, it still clears the
+-- orange highlights!
 vim.keymap.set("n", "<Esc>", function()
   vim.cmd.nohlsearch()
   vim.cmd.echo()
