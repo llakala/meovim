ae543d48881b5448d2e6b59ce83b944cadbc23f0
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-09-12 17:52:40 -0400

    luasnip: add signcolumn indicator

--- nvim/plugin/luasnip.lua
+++ nvim/plugin/luasnip.lua
@@ -1,10 +1,41 @@
 local ls = require("luasnip")
 
+local group_name = "LuasnipSignGroup"
+local group = vim.api.nvim_create_augroup(group_name, {})
+vim.fn.sign_define("LuasnipNodeLocation", {
+  text = "",
+  texthl = "DiagnosticOk",
+})
+
+-- Put indicator on the line of the current snippet node. Good way of seeing
+-- if you're still within a snippet and don't realize it
+vim.api.nvim_create_autocmd("User", {
+  group = group,
+  pattern = { "LuasnipInsertNodeEnter", "LuasnipChoiceNodeEnter" },
+  callback = function(a)
+    local node = ls.session.event_node
+    vim.fn.sign_place(0, group_name, "LuasnipNodeLocation", a.buf, {
+      lnum = node:get_buf_position()[1] + 1,
+      priority = 13,
+    })
+  end,
+})
+vim.api.nvim_create_autocmd("User", {
+  group = group,
+  pattern = { "LuasnipInsertNodeLeave", "LuasnipChoiceNodeLeave" },
+  callback = function(a)
+    vim.fn.sign_unplace(group_name, {
+      buffer = a.buf,
+    })
+  end,
+})
+
 ls.setup({
   enable_autosnippets = true,
 })
 
--- imap bindings are done in blink.cmp
+-- imap bindings are done in blink.cmp. TODO: use something other than <Tab>,
+-- see https://github.com/L3MON4D3/LuaSnip/issues/953
 vim.cmd([[
   smap <silent><expr> <Tab> luasnip#jumpable(1) ? '<Plug>luasnip-jump-next' : '<Tab>'
   smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
