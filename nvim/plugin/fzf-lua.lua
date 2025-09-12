5fc0cad95ba4fcde85359d5220179edc97bd35d5
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-09-12 18:50:21 -0400

    fzf-lua: remove vi-mode for buffer picker
    The control bindings fulfill my need for a true vi-mode, while also
    letting me type to pick. Still think the setup is very cool though -
    off to the `old` repo it goes.

--- nvim/plugin/fzf-lua.lua
+++ nvim/plugin/fzf-lua.lua
@@ -67,7 +67,7 @@ require("fzf-lua").setup({
       true, -- Inherit from default fzf keybinds
       jump = "accept",
 
-      -- Normal (ish) mode keybinds. The true normal mode is used in `buffers`
+      -- Normal (ish) mode keybinds.
       ["ctrl-j"] = "down",
       ["ctrl-k"] = "up",
       ["ctrl-l"] = "accept",
@@ -94,8 +94,6 @@ require("fzf-lua").setup({
       ["--header-lines"] = false,
     },
 
-    -- TODO: find a way to start on the second item
-
     actions = {
       ["ctrl-x"] = {
         reload = true,
@@ -103,37 +101,10 @@ require("fzf-lua").setup({
       },
     },
 
-    -- Start in normal mode
     keymap = {
       fzf = {
         -- Buffer picker shouldn't start on the current buffer
         load = "pos(2)",
-
-        j = "down",
-        k = "up",
-        l = "accept",
-        f = "jump",
-
-        -- Normal mode is on by default - calling this event will toggle it.
-        -- Ideally, we would have some `toggle-input` event, but as we lack
-        -- that, we use click-header (if I'm clicking in fzf, I'm doing
-        -- something wrong). This lets us toggle modes without having to write
-        -- out the keys in every single bind
-        ["click-header"] = "toggle-bind(j,k,l,f,i)",
-
-        -- If we're in insert mode, then pressing esc should take us to normal
-        -- mode. If we're NOT in insert mode, we must already be in normal mode,
-        -- and esc should quit fzf.
-        esc = 'transform:[[ "$FZF_INPUT_STATE" = enabled ]] && echo "hide-input+trigger(click-header)" || echo abort',
-
-        -- We have a true normal mode, let's use it!
-        start = "hide-input+unbind(alt-j,alt-k,alt-l,alt-f)",
-
-        -- From normal mode, enter insert mode. After doing this, we want to
-        -- immediately unmap it so we can actually type `i`. So we trigger
-        -- `click-header` and disable all the normal mode keymaps (including
-        -- this one).
-        i = "show-input+trigger(click-header)",
       },
     },
   },
