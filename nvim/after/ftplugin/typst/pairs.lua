f05aa873aafbf5f1c8aeac48f063dea6087f52c6
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-09-19 00:18:55 -0400

    ftplugin/typst/pairs: use generic `in_group` function
    This also comes with some group improvements - we now don't trigger
    these autopairs in `#{}` expressions either, and trigger when within
    manually-triggered markup with #[].

--- nvim/after/ftplugin/typst/pairs.lua
+++ nvim/after/ftplugin/typst/pairs.lua
@@ -2,33 +2,39 @@ local Rule = require("nvim-autopairs.rule")
 local cond = require("nvim-autopairs.conds")
 local ts_conds = require("nvim-autopairs.ts-conds")
 
--- Check recursively whether we're in math mode, rather than just assuming the
--- current scope is correct (we could be within a function or parentheses).
--- Referenced from
+-- Check recursively whether our current node is in some set of allowed nodes,
+-- and not in some set of disallowed nodes.
+-- Referenced from:
 -- 1: https://github.com/CaptainKills/dotfiles/blob/20d7d30f8507280795f5f14014752b40f7c7eff0/nvim/luasnippets/typst.lua#L22
 -- 2: https://github.com/ThetaOmega01/dotfiles/blob/a16df1873bb1f75e8bee2d59fc4c1ea48e7fd252/.config/nvim/lua/snippets/typst.lua#L15
-local is_in_math = function(_)
+local function in_group(allowed, disallowed)
   local ts = require("nvim-treesitter.ts_utils")
   local node = ts.get_node_at_cursor()
 
-  -- String within math mode shouldn't trigger snippets
-  if node and node:type() == "string" then
-    return false
-  end
-
   while node do
     local type = node:type()
-    if type == "math" then
+
+    -- Sometimes these are within math - if we see these before we see math,
+    -- we're currently in them, and should exit early
+    if vim.tbl_contains(disallowed, type) then
+      return false
+    end
+    if vim.tbl_contains(allowed, type) then
       return true
     end
+
     node = node:parent()
   end
 
   return false
 end
 
-local not_in_math = function(_)
-  return not is_in_math()
+local in_math = function(_)
+  return in_group({ "math" }, { "content", "string", "code" })
+end
+
+local in_content = function(_)
+  return in_group({ "content", "string" }, { "math", "code" })
 end
 
 require("nvim-autopairs").add_rules({
@@ -41,22 +47,22 @@ require("nvim-autopairs").add_rules({
   Rule("$", "$", "typst")
     :with_pair(cond.not_after_regex("[%w]"))
     :with_pair(ts_conds.is_not_ts_node("math"))
-    :with_move(is_in_math)
+    :with_move(in_math)
     :replace_map_cr(function()
       return "<C-g>u<CR><ESC>O<Tab>"
     end),
 
-  -- Only pair these when we're not in math mode
+  -- Only pair these when we're writing markup
   Rule("*", "*", "typst")
     :with_pair(cond.not_before_regex("[%w]"))
     :with_pair(cond.not_after_regex("[%w]"))
-    :with_pair(not_in_math),
+    :with_pair(in_content),
   Rule("_", "_", "typst")
     :with_pair(cond.not_before_regex("[%w]"))
     :with_pair(cond.not_after_regex("[%w]"))
-    :with_pair(not_in_math),
+    :with_pair(in_content),
   Rule("<", ">", "typst")
     :with_pair(cond.not_before_regex("[%w]"))
     :with_pair(cond.not_after_regex("[%w]"))
-    :with_pair(not_in_math),
+    :with_pair(in_content),
 })
