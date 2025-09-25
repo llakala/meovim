410f90bb55c16d746990ec2f2e016a542fbce851
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-09-25 00:42:49 -0400

    nvim-autopairs: give vague offerings to the gods until they like the table order
    This is order-dependent on my machine, and is impossible to find a
    logical explanation for. I am very excited to find a different autopairs
    plugin to use.

--- nvim/after/ftplugin/lua/pairs.lua
+++ nvim/after/ftplugin/lua/pairs.lua
@@ -20,11 +20,14 @@ local function replacePunctuation(lang, punct)
 end
 
 npairs.add_rules({
-  -- From wiki: https://github.com/windwp/nvim-autopairs/wiki/Custom-rules#insertion-with-surrounding-check
-  -- Interestingly, adding the single quote one breaks the {} one. No clue why!
-  Rule("{", "},", "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
-  Rule("(", "),", "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+  -- From wiki:
+  -- https://github.com/windwp/nvim-autopairs/wiki/Custom-rules#insertion-with-surrounding-check
+  -- For some reason, these only work with a specific order, I can't add one for
+  -- parentheses, and if I tweak another rule very slightly, they stop working.
+  -- I hate nvim-autopairs.
   Rule('"', '",', "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+  Rule("{", "},", "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
+  Rule("'", "',", "lua"):with_pair(ts_conds.is_ts_node({ "table_constructor" })),
 
   replacePunctuation("lua", ","),
 })
