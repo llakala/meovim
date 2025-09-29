ed86cb4cc14382c03d176f4b8f9c4f4ac019e94d
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-09-29 18:13:28 -0400

    mini: use single comment textobject, differentiated by operator
    I saw someone else's textobject using `vim.v.operator` for setting
    different behavior depending on the mode. In theory, textobjects aren't
    supposed to do this - the different operators are just choosing what
    happens with the selected text. But I think having a single textobject
    here has more benefits than it does drabacks.
    
    There are two categories of things I do with comments:
    - change it
    - delete it / something else
    
    When changing a commentt, I want to keep the comment header, so I can
    add some new content. When doing something else, I want the header
    included. Before, I implemented this through two separate textobjects -
    you could do `cic` to "change inside comment", and `dac` to "delete
    around comment". But you'd only ever use the `ic` textobject for changng
    a comment - so why not just unify them to one? Thanks to `vim.operator`,
    we can do this.
    
    In the new system, we just have `ic`. `cic` will keep the header, while
    `dic`, `vic`, `gwic`, etc. will remove it. This hypothetically breaks
    the unwritten textobject rules, as you're not supposed to do `i` version
    without an `a` equivalent.
    
    As part of this, we moved the implemention to not depend on `mini.ai`. I
    didn't actually have anything that required it, and using the vanilla
    mechanisms means there's less things that can break.
    
    Finally, we did an extra bit of conditional logic for EOL comments. If
    we only have two categories - change and non-change - then `vic` would
    include the initial space between the code and the comment. This isn't
    helpful if I just want to copy a comment, so we only include that
    initial space when using the `d` operator.

--- nvim/plugin/mini/comment.lua
+++ nvim/plugin/mini/comment.lua
@@ -1,3 +1,7 @@
+-- we keep `vgc` and `gcip`, so you can comment/uncomment some range. The custom
+-- textobject is only for modifying some commented range
+vim.keymap.del("o", "gc")
+
 require("mini.comment").setup({
   mappings = {
     comment = "",
@@ -5,11 +9,108 @@ require("mini.comment").setup({
     comment_visual = "#",
 
     -- We've homerolled a cooler comment textobject that gives separate behavior
-    -- with `ic` vs `ac`, and alsp handles EOL comments! Check it out here:
-    -- https://github.com/llakala/meovim/blob/main/nvim/plugin/mini/ai.lua
+    -- with `dic` vs `cic`, and also handles EOL comments!
     textobject = "",
   },
 })
 
-vim.keymap.del("o", "gc")
-vim.keymap.del("n", "gcc")
+-- From https://github.com/echasnovski/mini.nvim/issues/1837
+local function select_multiline_comment(operator)
+  require("mini.comment").textobject()
+
+  -- Default behavior of mini.comment removes the start of the comment, which is
+  -- fine for most cases. We only keep going if we're doing anything other than
+  -- `cic`
+  -- TODO: is checking mode unnecessary here?
+  if vim.fn.mode() ~= "V" or operator ~= "c" then
+    return
+  end
+
+  -- Adjust selection to be charwise and not include edge comment parts
+  local comment_left, comment_right = vim.bo.commentstring:match("^(.*)%%s(.*)$")
+  if comment_left == nil then
+    return
+  end
+
+  -- NOTE: this depends on implementation detail that `MiniComment.textobject`
+  -- puts cursor on last line of comment block
+  local from_line, to_line = vim.fn.line("v"), vim.fn.line(".")
+
+  vim.fn.feedkeys("v", "nx")
+  local to_col = vim.fn.getline(to_line):find(vim.pesc(comment_right) .. "%s*$")
+  vim.api.nvim_win_set_cursor(0, { to_line, to_col - 2 })
+
+  vim.fn.feedkeys("o", "nx")
+  local _, from_col = vim.fn.getline(from_line):find("^%s*" .. vim.pesc(comment_left))
+  vim.api.nvim_win_set_cursor(0, { from_line, from_col })
+  vim.fn.feedkeys("o", "nx")
+end
+
+local function select_eol_comment(operator, line, commentstr)
+  -- These represents the beginning and the end of the commentstring. We'll
+  -- choose one of them to use depending on `include_header`. commentstr is
+  -- pre-escaped, so we don't need to bother with escaping it
+  local comment_start, comment_end = line:find(commentstr)
+
+  -- This represents the commentstr not being found in the line. Not sure how
+  -- the end index could be nil if the start index wasn't, but better safe than
+  -- sorry
+  if comment_start == nil or comment_end == nil then
+    return nil
+  end
+
+  -- This is only for EOL comments, since mini.comment handles the other ones.
+  -- Therefore, we can assume the comment will only be on one line, and can
+  -- reuse the current line number. And no, there's no support for doc comments
+  -- like /* */ - mini.comment doesn't support them, and I can only handle so
+  -- much regex.
+  local line_num = vim.fn.line(".")
+
+  local from_col = nil
+
+  -- We include the comment in the selection if it was `dgc`, but leave it out
+  -- if it was `cgc`
+  -- TODO: add smarter range logic for `d`, so we only include whitespace before
+  -- comment header if it exists
+  if operator == "c" then
+    from_col = comment_end
+  elseif operator == "d" then
+    from_col = comment_start - 2
+  else
+    from_col = comment_start - 1
+  end
+
+  -- Till the end of the line, not including final newline
+  local to_col = line:len() - 1
+
+  -- exit visual mode if we're currently in it, so we can select a range properly
+  -- \22 is <C-v>, \27 is <Esc>
+  if vim.tbl_contains({ "v", "V", "\22" }, vim.fn.mode()) then
+    vim.cmd("normal! \27")
+  end
+
+  --
+  vim.api.nvim_win_set_cursor(0, { line_num, from_col })
+  vim.cmd("normal! v")
+  vim.api.nvim_win_set_cursor(0, { line_num, to_col })
+end
+
+vim.keymap.set({ "x", "o" }, "ic", function()
+  local operator = vim.fn.mode() == "v" and "v" or vim.v.operator
+
+  local line = vim.api.nvim_get_current_line()
+
+  -- Get the commentstring up until %s (including the space). Then, escape it
+  -- for use in lua regex
+  local commentstr = vim.pesc(vim.bo.commentstring:match("^(.*)%%s"))
+
+  -- If the line has a comment that's only following whitespace, we can
+  -- defer to mini.comment. We only have custom logic for EOL comments,
+  -- since mini.comment doesn't implement logic for them
+  if line:match("^%s*" .. commentstr) then
+    select_multiline_comment(operator)
+    return
+  end
+
+  select_eol_comment(operator, line, commentstr)
+end)
