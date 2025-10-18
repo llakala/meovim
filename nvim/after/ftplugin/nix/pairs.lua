56e3c2bc643835b13a9c98550e4d2e48e3f434a0
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-18 17:01:27 -0400

    ftplugin/nix: homeroll semicolon logic using anonymous children
    See comment! I can't get this to fail - it works in all attrsets, it
    works in let exprs, it doesn't trigger for comparisons like `==`, it
    doesn't trigger within other syntax elements. Thank you, anonymous
    children!

--- nvim/after/ftplugin/nix/pairs.lua
+++ nvim/after/ftplugin/nix/pairs.lua
@@ -1,13 +1,26 @@
 local npairs = require("nvim-autopairs")
 local Rule = require("nvim-autopairs.rule")
+local ts = require("nvim-treesitter.ts_utils")
 
 npairs.add_rules({
-  -- The space in `= ` is loadbearing here - without it, nix won't recognize us
-  -- as being within an attrset for the first attr. We use my custom util
-  -- function that recurses up the node tree, rather than just checking the
-  -- current node
-  Rule("= ", ";", "nix"):with_pair(function(_)
-    Autopairs_utils.in_ts_group({ "binding_set", "attrset_expression" }, {})
+  -- Homerolled logic to auto-add semicolons. I didn't think most nodes even had
+  -- children, but it turns out there are anonymous nodes that don't show up in
+  -- `:InspectTree` until you type `a` - and these anonymous children seem to
+  -- contain the wrapping syntax elements for your current node. All the
+  -- existing setups for auto-semicolons had edge cases - but I haven't been
+  -- able to find a place where this fails.
+  Rule("=", ";", "nix"):with_pair(function(_)
+    local node = vim.treesitter.get_node()
+    if node == nil then
+      return false
+    end
+    local child = node:child(0)
+    if child == nil then
+      return false
+    end
+    vim.print(child:type())
+    return vim.tbl_contains({ "{", "let" }, child:type())
   end),
+
   Autopairs_utils.replacePunctuation("nix", ";"),
 })
