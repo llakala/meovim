144871ccf5fb3311a8715271916f344a348257b6
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-18 15:46:28 -0400

    ftplugin/nix: make semicolon autopair work in `let` expr
    With the default `is_ts_node`, I wasn't getting semicolons in let exprs.
    Thanks to `is_ts_node` searching up the tree, it now seems to work
    perfectly!

--- nvim/after/ftplugin/nix/pairs.lua
+++ nvim/after/ftplugin/nix/pairs.lua
@@ -1,9 +1,12 @@
 local npairs = require("nvim-autopairs")
 local Rule = require("nvim-autopairs.rule")
-local ts_conds = require("nvim-autopairs.ts-conds")
 
 npairs.add_rules({
   -- The space in `= ` is loadbearing here - without it, nix won't recognize us
-  -- as being within an attrset for the first attr
-  Rule("= ", ";", "nix"):with_pair(ts_conds.is_ts_node({ "binding_set", "attrset_expression" })),
+  -- as being within an attrset for the first attr. We use my custom util
+  -- function that recurses up the node tree, rather than just checking the
+  -- current node
+  Rule("= ", ";", "nix"):with_pair(function(_)
+    Autopairs_utils.in_ts_group({ "binding_set", "attrset_expression" }, {})
+  end),
 })
