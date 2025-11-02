18bceea191a669ac1dfff5cb7526ab7d06a482e0
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-01 22:50:19 -0400

    plugin/typst-preview: move to typst ftplugin
    Also not calling the setup hook anymore.

--- nvim/after/ftplugin/typst/typst.lua
+++ nvim/after/ftplugin/typst/typst.lua
@@ -1,4 +1,7 @@
 o.textwidth = 120
 vim.opt_local.formatoptions:append({ t = true }) -- Wrap all text, not just comments
 
+-- We don't call `setup` since it does its own binary nonsense - we just need
+-- the commands to exist
+require("typst-preview.commands").create_commands()
 vim.keymap.set("n", "<leader>p", vim.cmd.TypstPreview, { buffer = true })
