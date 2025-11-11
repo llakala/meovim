ecf69cbaf71bc6a1102af39df4ed1d6c638d3fc2
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-11 00:52:59 -0500

    startPlugins/fFtT-highlights: update rev

--- packages/startPlugins/fFtT-highlights-nvim.nix
+++ packages/startPlugins/fFtT-highlights-nvim.nix
@@ -10,7 +10,7 @@
   src = fetchFromGitHub {
     owner = "samiulsami";
     repo = "fFtT-highlights.nvim";
-    rev = "03dd7d8c9a9c8bc45d35ca47493c9cd97073feb7";
-    hash = "sha256-Xf+xuLI17ip88D8R5fC2V/cKFulos1+bzjAXKbBDrbw=";
+    rev = "b66d65e105cb846d0507a6e80d904f9ee0377b06";
+    hash = "sha256-HOfcfsAcnDMsy8epX6R9TPzjFIbvVsLGmfpv2pMsi68=";
   };
 }
