9c39b7ba72f1283cb87786fad88104794d08c260
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-21 13:07:26 -0500

    autocmds/yank: don't move cursor on yank
    See https://web.archive.org/web/20250327223946/https://nanotipsforvim.prose.sh/sticky-yank!

--- nvim/lua/config/autocmds/yank.lua
+++ nvim/lua/config/autocmds/yank.lua
@@ -1,7 +1,20 @@
+vim.keymap.set({ "n", "x" }, "y", function()
+  vim.b.cursor_pre_yank = vim.api.nvim_win_get_cursor(0)
+  return "y"
+end, { expr = true })
+vim.keymap.set("n", "Y", function()
+  vim.b.cursor_pre_yank = vim.api.nvim_win_get_cursor(0)
+  return "y$"
+end, { expr = true })
+
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
+  desc = "Highlight when yanking, and keep cursor in the same place",
   callback = function()
     vim.hl.on_yank()
+    if vim.v.event.operator == "y" and vim.b.cursor_pre_yank ~= nil then
+      vim.api.nvim_win_set_cursor(0, vim.b.cursor_pre_yank)
+      vim.b.cursor_pre_yank = nil
+    end
   end,
-  group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+  group = vim.api.nvim_create_augroup("YankLogic", { clear = true }),
 })
