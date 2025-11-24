336839ffd6951065d0c8c6db341120068305e418
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-24 02:32:02 -0500

    ftplugin/nix/pairs: rework `;` logic
    I like the new way things are done. Before, typing `;` if it was already
    there would mean it would just "type over" the existing semicolon. This
    was specifically annoying if you tried to type something like:
    
    ```
    {
      x = with pkgs; [ git ];
    }
    ```
    
    The first semicolon would be auto-inserted on `= `, and then when you
    tried to add one yourself for `with`, it would overwrite the existing
    one, so you'd have one when you needed two.
    
    Now, if there's already a semicolon after your cursor, it won't add
    another one - but if you manually type a semicolon, it'll actually be
    entered.
    
    We also add auto-semicolon for `inherit`, and prevent `!= ` from
    triggering a semicolon. I tried to do the same for `==`, but wasn't able
    to get it to work.

--- nvim/after/ftplugin/nix/pairs.lua
+++ nvim/after/ftplugin/nix/pairs.lua
@@ -1,13 +1,18 @@
 local npairs = require("nvim-autopairs")
 local Rule = require("nvim-autopairs.rule")
+local cond = require("nvim-autopairs.conds")
+
+local auto_semicolon = function(prefix)
+  return Rule(prefix, ";", "nix")
+    -- Don't add another semicolon if there already is one after the cursor
+    :with_pair(cond.not_after_regex(";", 2))
+    -- If we're backspacing, we don't want to get rid of the whole thing
+    :with_del(function(_)
+      return false
+    end)
+end
 
 npairs.add_rules({
-  -- I tried going the treesitter route for this, but found there were always
-  -- going to be edge cases. Sure, you might type `=` inside a string, but the
-  -- chance of typing `= ` is much lower.
-  Rule("= ", ";", "nix"):with_del(function(_)
-    return false
-  end),
-
-  Autopairs_utils.replacePunctuation("nix", ";"),
+  auto_semicolon("= "):with_pair(cond.not_before_regex("!", 2)),
+  auto_semicolon("inherit "),
 })
