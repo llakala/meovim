04acf3b55fafa1c1be075efa67a67828f224f2e8
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-24 01:46:02 -0500

    lsp/nil: disable autoArchive for inputs

--- nvim/after/lsp/nil_ls.lua
+++ nvim/after/lsp/nil_ls.lua
@@ -1,5 +1,14 @@
 ---@type vim.lsp.Config
 return {
+  settings = {
+    ["nil"] = {
+      nix = {
+        flake = {
+          autoArchive = false,
+        },
+      },
+    },
+  },
   on_attach = function(client)
     -- We get completion from nixd, and everything else from nil
     client.server_capabilities.completionProvider = nil
