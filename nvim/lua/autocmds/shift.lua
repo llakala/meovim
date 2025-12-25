09e18d5cab1e23dfb17f4a4ee4c31f918de29181
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-12-25 09:26:09 -0500

    autocmds/shift: add explanatory comment

--- nvim/lua/autocmds/shift.lua
+++ nvim/lua/autocmds/shift.lua
@@ -7,6 +7,14 @@ vim.keymap.set("n", ">", function()
   return ">"
 end, { expr = true })
 
+-- To move the cursor after <$motion or >$motion, our best bet is to use
+-- TextChanged, since there isn't some kind of OperatorPerformed textobject.
+-- Most of the time, querying `v:operator` lets us check if we've actually just
+-- shifted. The only exception I've found so far is undoing - so we prevent the
+-- condition from triggering here.
+--
+-- In the future, I'd like to find a better way of doing this than TextChanged -
+-- maybe it's possible to wrap the </> binds.
 vim.keymap.set("n", "u", function()
   vim.b.cursor_pre_shift = nil
   return "u"
