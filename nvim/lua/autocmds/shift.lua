bb15049a098754f3437483150c10cf635366301b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-12-25 09:16:20 -0500

    autocmds/shift: don't trigger on `u`
    Checking v:operator seemed to work for almost all cases, but `<<u` was
    causing an issue where it triggered TextChanged, but didn't update the
    operator. An OperatorPerformed textobject would make this a lot
    easier... ah well.

--- nvim/lua/autocmds/shift.lua
+++ nvim/lua/autocmds/shift.lua
@@ -7,12 +7,18 @@ vim.keymap.set("n", ">", function()
   return ">"
 end, { expr = true })
 
+vim.keymap.set("n", "u", function()
+  vim.b.cursor_pre_shift = nil
+  return "u"
+end, { expr = true })
+
 vim.api.nvim_create_autocmd("TextChanged", {
   desc = "Keep cursor in the same place when shifting text",
   callback = function()
     local operator = vim.v.operator
-    if operator == ">" or operator == "<" then
-      local row, col = unpack(vim.b.cursor_pre_shift)
+    local pos = vim.b.cursor_pre_shift
+    if (operator == ">" or operator == "<") and pos ~= nil then
+      local row, col = unpack(pos)
       local shiftwidth = vim.fn.shiftwidth()
       local sign = operator == ">" and 1 or -1
       local new_pos = { row, col + (sign * shiftwidth) }
