d6af66171bc8a042a1fd0d9e4db1d2b2b8806218
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-01-06 00:54:10 -0500

    autocmds/shift: add o and O workarounds

--- nvim/lua/autocmds/shift.lua
+++ nvim/lua/autocmds/shift.lua
@@ -10,14 +10,22 @@ end, { expr = true })
 -- To move the cursor after <$motion or >$motion, our best bet is to use
 -- TextChanged, since there isn't some kind of OperatorPerformed textobject.
 -- Most of the time, querying `v:operator` lets us check if we've actually just
--- shifted. The only exception I've found so far is undoing - so we prevent the
--- condition from triggering here.
---
+-- shifted. However, there are a few exceptions to this - I've found u, o, and
+-- O need to be worked around.
+
 -- In the future, I'd like to find a better way of doing this than TextChanged -
 -- maybe it's possible to wrap the </> binds.
 vim.keymap.set("n", "u", function()
   vim.b.cursor_pre_shift = nil
   return "u"
+end, { expr = true })
+vim.keymap.set("n", "o", function()
+  vim.b.cursor_pre_shift = nil
+  return "o"
+end, { expr = true })
+vim.keymap.set("n", "O", function()
+  vim.b.cursor_pre_shift = nil
+  return "O"
 end, { expr = true })
 
 vim.api.nvim_create_autocmd("TextChanged", {
