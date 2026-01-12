520b74a1e8152a126b32d82108eeb703803e6a47
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-01-12 17:23:09 -0500

    globals/dynamic_indent: init
    Now my time writing inline Nix in markdown will be less painful!

--- nvim/plugin/plugins.lua
+++ nvim/plugin/plugins.lua
@@ -8,6 +8,8 @@ require("rainbow-delimiters.setup").setup({
   },
 })
 
+require("globals/dynamic_indent").setup({ pattern = "*.md" })
+
 -- Enable synchronous treesitter parsing to prevent flashing
 vim.g._ts_force_sync_parsing = true
 vim.api.nvim_create_autocmd("FileType", {
