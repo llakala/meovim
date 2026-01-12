9f98f0d8d7ac1cd0d8eacbe502e582e5ba57212e
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-01-12 17:22:05 -0500

    plugins: fix treesitter startup
    Turns out it hasn't been working!

--- nvim/plugin/plugins.lua
+++ nvim/plugin/plugins.lua
@@ -7,3 +7,12 @@ require("rainbow-delimiters.setup").setup({
     "RainbowDelimiterGreen",
   },
 })
+
+-- Enable synchronous treesitter parsing to prevent flashing
+vim.g._ts_force_sync_parsing = true
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = vim.treesitter.language._complete(),
+  callback = function()
+    vim.treesitter.start()
+  end,
+})
