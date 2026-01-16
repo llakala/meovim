559864283946878d91fe1bb8ce5ba4c38361d975
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-01-15 22:03:11 -0500

    oil: delete buffer when file deleted
    Wonderful to see that someone already did this!

--- nvim/after/ftplugin/oil.lua
+++ nvim/after/ftplugin/oil.lua
@@ -7,3 +7,35 @@ vim.cmd([[
   :cabbrev <buffer> <silent> w WriteOil
   :cabbrev <buffer> <silent> wq execute 'WriteOil' <bar> qa
 ]])
+
+-- From https://github.com/stevearc/oil.nvim/issues/310#issuecomment-2019214285
+-- Deletes a buffer when the
+vim.api.nvim_create_autocmd("User", {
+  pattern = "OilActionsPost",
+  callback = function(args)
+    local parse_url = function(url)
+      return url:match("^.*://(.*)$")
+    end
+
+    if args.data.err then
+      return
+    end
+
+    for _, action in ipairs(args.data.actions) do
+      if action.type == "delete" and action.entry_type == "file" then
+        local path = parse_url(action.url)
+        local bufnr = vim.fn.bufnr(path)
+        if bufnr == -1 then
+          return
+        end
+
+        local winnr = vim.fn.win_findbuf(bufnr)[1]
+        if not winnr then
+          return
+        end
+
+        vim.fn.win_execute(winnr, "bfirst | bd" .. bufnr)
+      end
+    end
+  end,
+})
