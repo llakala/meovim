e2ce4259ef18ed5ccd550c6054b9f809de53c96b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-01-16 00:16:32 -0500

    autocmds/shiftwidth: cleanup
    The operator stuff is a remnant of when I didn't realize I could
    just set `vim.b.cursor.pre_shift = nil` right after.

--- nvim/lua/autocmds/shift.lua
+++ nvim/lua/autocmds/shift.lua
@@ -10,13 +10,13 @@ end, { expr = true })
 vim.api.nvim_create_autocmd("TextChanged", {
   desc = "Keep cursor in the same place when shifting text",
   callback = function()
-    local operator = vim.v.operator
     local pos = vim.b.cursor_pre_shift
-    if (operator == ">" or operator == "<") and pos ~= nil then
+    if pos ~= nil then
       local row, col = unpack(pos)
       local shiftwidth = vim.fn.shiftwidth()
-      local sign = operator == ">" and 1 or -1
+      local sign = vim.v.operator == ">" and 1 or -1
       local new_pos = { row, math.max(0, col + (sign * shiftwidth)) }
+
       vim.api.nvim_win_set_cursor(0, new_pos)
       vim.b.cursor_pre_shift = nil
     end
