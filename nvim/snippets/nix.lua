3ab89e872e1418d57b3e1aadc7ba81cc2eeff88e
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-01-31 17:28:35 -0500

    snippets/nix: make `let` not trigger in comments

--- nvim/snippets/nix.lua
+++ nvim/snippets/nix.lua
@@ -1,3 +1,14 @@
+local not_in_comment = function()
+  local _, row, col, _ = unpack(vim.fn.getpos("."))
+  local node = vim.treesitter.get_node({
+    pos = { row - 1, col - 2 },
+  })
+  return node and node:type() ~= "comment"
+end
+
 return {
-  s("let", fmt("let\n  {}\nin ", { i(1) })),
+  s({
+    trig = "let",
+    show_condition = not_in_comment,
+  }, fmt("let\n  {}\nin ", { i(1) }), {}),
 }
