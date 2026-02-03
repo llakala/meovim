e12e61c357dc683c596f05f8812ec0d1b7633af1
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-02-02 20:21:19 -0500

    ftplugin/nix: move comment down

--- nvim/after/ftplugin/nix/textobjects.lua
+++ nvim/after/ftplugin/nix/textobjects.lua
@@ -1,8 +1,8 @@
 local nvim_surround = require("nvim-surround")
-local utils = require("nvim-surround.config")
--- See https://github.com/kylechui/config.nvim/blob/22a3a13f348aaa49ddd2a498fb03991936b20464/after/ftplugin/nix.lua
+
 nvim_surround.buffer_setup({
   surrounds = {
+    -- See https://github.com/kylechui/config.nvim/blob/22a3a13f348aaa49ddd2a498fb03991936b20464/after/ftplugin/nix.lua
     ["$"] = {
       find = "$%b{}",
       add = { "${", "}" },
