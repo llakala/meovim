1b9fb43a0f46a75421115612f73ebf9141419317
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-04-06 13:23:54 -0400

    startPlugins/canola: bump version
    Fixes another bug I reported.

--- packages/startPlugins/canola-nvim.nix
+++ packages/startPlugins/canola-nvim.nix
@@ -6,7 +6,7 @@
   src = fetchFromGitHub {
     owner = "barrettruth";
     repo = "canola.nvim";
-    rev = "6176bccce471937e43375bf9595cd9b458ee838c";
-    hash = "sha256-aIzv6olxvhZaLVDL0bEW9vIkbZ22BvUEeZ4SsDCaSeg=";
+    rev = "2ae54d925e2830601840612a21c1574a4c573266";
+    hash = "sha256-g9LkyMnAMHsQo9kkgybFsiP7UGrpcu88wEt6S+oSP3A=";
   };
 }
