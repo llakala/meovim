d4e483f835e00b4f5c88b0c542b43acb1ec8eb81
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-04-07 23:38:47 -0400

    default.nix: prefix name of config plugin with underscore
    Hit a weird issue where my Canola config was applying in my devmode, but
    not outside of it. Turns out my config was being read as a plugin named
    `config`. This meant canola would be loaded first, and since it just
    uses vim.g for its config, it wouldn't see my config values being set.
    With the devmode, mnw must've handled my config loading beforehand,
    preventing the bug from surfacing.

--- default.nix
+++ default.nix
@@ -24,7 +24,10 @@ mnw.lib.wrap pkgs {
     startAttrs = import ./packages/startPlugins.nix args;
     start = import ./packages/treesitter.nix args;
     optAttrs = import ./packages/optPlugins.nix args;
-    dev.config = {
+    # Prefixing the name with _ to read my config first when iterating through
+    # all `plugin/` files. This prevents plugins from trying to read vim.g
+    # values before they've been set (looking at you, canola)
+    dev._config = {
       pure = ./nvim;
       impure = "/home/emanresu/Documents/projects/meovim/nvim"; # Absolute path needed
     };
