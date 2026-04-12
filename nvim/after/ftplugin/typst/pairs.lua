a2cb561c5ddc9571a9b4498088383c9b146af587
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-04-11 23:28:23 -0400

    ftplugin/typst/pairs: only allow moving past second pair
    Tried a lot of ways to only move right when the cursor is at `$|$` or
    `$ 5 |$`, but not |$$ or |$ 5 $|. This should be trivial - just check
    the current node! But the treesitter grammar has shitty behavior here
    and counts you as being in the node when you're right before it. Unless
    it's the only node, then it works sanely. I tried a ton of workarounds,
    but it turned out the best solution was using the shitty behavior to my
    advantage. I can't find any edge cases with this - but maybe I'll find
    some when I actually get around to using it.

--- nvim/after/ftplugin/typst/pairs.lua
+++ nvim/after/ftplugin/typst/pairs.lua
@@ -1,13 +1,32 @@
 local Rule = require("nvim-autopairs.rule")
 local cond = require("nvim-autopairs.conds")
-local ts_conds = require("nvim-autopairs.ts-conds")
 
-local in_math = function(_)
-  return Custom.in_ts_group({ "math" }, { "content", "string", "code" })
-end
+-- Groups that are allowed/disallowed for triggering a pair
+local math = {
+  {},
+  { "content", "string", "code" },
+}
+local content = {
+  { "content" },
+  { "math", "string", "code" },
+}
 
-local in_content = function(_)
-  return Custom.in_ts_group({ "content", "string" }, { "math", "code" }, true)
+-- We want to only allow moving past a delimiter if it's the second one. This
+-- should be trivial, but the typst treesitter grammar has really stupid
+-- behavior with the current node. If you're right before some block, it counts
+-- as being inside it! See my issue report about this:
+-- https://github.com/uben0/tree-sitter-typst/issues/53
+
+-- However, we can actually abuse this to fix our problem. If we're inside a set
+-- of $$, the current node will be an anonymous $ (the second one). We only
+-- allow moving forward if we have a previous sibling, which we will - either
+-- the first $, or some formula) However, if we were BEFORE the $$, we'd be
+-- looking at the first $, but there would be no previous sibling.
+local function can_move_past(char)
+  return function()
+    local node = vim.treesitter.get_node({ include_anonymous = true })
+    return node and node:type() == char and node:prev_sibling() ~= nil
+  end
 end
 
 require("nvim-autopairs").add_rules({
@@ -16,21 +35,35 @@ require("nvim-autopairs").add_rules({
   Rule('"', '"', { "typst" }):with_pair(cond.after_text("$")),
 
   Rule("$", "$", "typst")
-    :with_pair(cond.not_after_regex("[%w]"))
-    :with_pair(ts_conds.is_not_ts_node("math"))
-    :with_move(in_math),
+    :with_pair(function()
+      return Custom.in_ts_group(math[1], math[2], true)
+    end)
+    -- only allow moving past the right $, not the left one
+    :with_move(function()
+      return can_move_past("$")
+    end),
 
-  -- Only pair these when we're writing markup
   Rule("*", "*", "typst")
-    :with_pair(cond.not_before_regex("[%w]"))
-    :with_pair(cond.not_after_regex("[%w]"))
-    :with_pair(in_content),
+    :with_pair(function()
+      return Custom.in_ts_group(content[1], content[2], true)
+    end)
+    -- only allow moving past the right *, not the left one
+    :with_move(can_move_past("*")),
+
   Rule("_", "_", "typst")
-    :with_pair(cond.not_before_regex("[%w]"))
-    :with_pair(cond.not_after_regex("[%w]"))
-    :with_pair(in_content),
+    :with_pair(function()
+      return Custom.in_ts_group(content[1], content[2], true)
+    end)
+    -- only allow moving past the right _, not the left one
+    :with_move(can_move_past("_")),
+
   Rule("<", ">", "typst")
-    :with_pair(cond.not_before_regex("[%w]"))
-    :with_pair(cond.not_after_regex("[%w]"))
-    :with_pair(in_content),
+    :with_pair(function()
+      return Custom.in_ts_group(content[1], content[2], true)
+    end)
+    -- No need for can_move_past, since < and > are distinguishable
+    :with_move(function()
+      local node = vim.treesitter.get_node({ include_anonymous = true })
+      return node and node:type() == ">"
+    end),
 })
