29a08f5c42a06aff58d9000715dd1242f348914e
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-05-24 00:17:24 -0400

    keybinds: allow moving windows without prefix

--- nvim/lua/keybinds.lua
+++ nvim/lua/keybinds.lua
@@ -10,7 +10,6 @@ vim.keymap.set("i", "<Esc>", "<Esc>l")
 vim.keymap.set("n", "U", "<C-r>", { desc = "Redo" })
 
 vim.keymap.set("n", "<leader><leader>", "<C-^>")
-vim.keymap.set("n", "<A-w>", "<C-w>") -- I have <C-w> to close a tab in Kitty. Should get rid of that!
 
 local ERROR = vim.diagnostic.severity.ERROR
 vim.keymap.set("n", "[e", function()
@@ -90,3 +89,9 @@ vim.keymap.set("n", "gP", '"*P')
 
 vim.keymap.set("n", "[u", "<Cmd>earlier 1f<CR>")
 vim.keymap.set("n", "]u", "<Cmd>later 1f<CR>")
+
+vim.keymap.set("n", "<A-w>", "<C-w>") -- I have <C-w> to close a tab in Kitty. Should get rid of that!
+vim.keymap.set("n", "<A-j>", "<C-w>j")
+vim.keymap.set("n", "<A-k>", "<C-w>k")
+vim.keymap.set("n", "<A-h>", "<C-w>h")
+vim.keymap.set("n", "<A-l>", "<C-w>l")
