8e6272144e1319a2e72ec4a4f8d982d10f320477
Eman Resu <elevenaka11@gmail.com>
2026-07-07 21:25:51 -0400

    commands/termhl: disable new tab

--- nvim/lua/commands/termhl.lua
+++ nvim/lua/commands/termhl.lua
@@ -2,8 +2,6 @@
 vim.api.nvim_create_user_command("TermHl", function()
   local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 
-  -- Open a new tab, so when we reopen neovim, it will still have the old one
-  vim.cmd("tabnew")
   local b = vim.api.nvim_create_buf(false, true)
 
   vim.api.nvim_set_option_value("buftype", "nofile", { buf = b })
