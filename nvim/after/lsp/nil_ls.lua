2303d9da41116012e03472cacbd018be2093dbdb
Eman Resu <elevenaka11@gmail.com>
2026-08-03 00:22:26 -0400

    nil: disable lsp-based incremental selection

--- nvim/after/lsp/nil_ls.lua
+++ nvim/after/lsp/nil_ls.lua
@@ -1,5 +1,10 @@
 ---@type vim.lsp.Config
 return {
+  on_init = function(client)
+    -- This gives basically equivalent results to treesitter, but with a few
+    -- annoying bugs
+    client.server_capabilities.selectionRangeProvider = nil
+  end,
   settings = {
     ["nil"] = {
       nix = {
