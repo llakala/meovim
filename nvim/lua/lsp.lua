548f53cb8b7fc044618f61ddf4cfa2be40bbb74d
Eman Resu <elevenaka11@gmail.com>
2026-08-03 00:14:36 -0400

    lsp: don't only run once, make mappings conditional

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,6 +1,10 @@
+vim.keymap.del("n", "grn")
+-- This is just ascii stuff by default - useless to me!
+vim.keymap.del({ "n", "x" }, "gra")
+
 vim.api.nvim_create_autocmd("LspAttach", {
-  once = true,
-  callback = function()
+  callback = function(event)
+    local args = { buf = event.buf }
     vim.lsp.inlay_hint.enable(true)
 
     vim.diagnostic.config({
@@ -14,19 +18,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
     -- Replace mode is stupid, and nobody sane would ever use it. If neovim can
     -- change K, I can change R.
-    vim.keymap.del("n", "grn")
     vim.keymap.set("n", "R", function()
       vim.g.input_normal_mode = true
       vim.lsp.buf.rename()
-    end, { desc = "Rename symbol" })
+    end, args)
 
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
-    vim.keymap.set("n", "grd", vim.diagnostic.setloclist)
-    vim.keymap.set("n", "grD", vim.diagnostic.setqflist)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, args)
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, args)
+    vim.keymap.set("n", "grd", vim.diagnostic.setloclist, args)
+    vim.keymap.set("n", "grD", vim.diagnostic.setqflist, args)
 
-    -- This is just ascii stuff by default - useless to me!
-    vim.keymap.del({ "n", "x" }, "gra")
-    vim.keymap.set({ "n", "x" }, "ga", vim.lsp.buf.code_action, { desc = "Code action" })
+    vim.keymap.set({ "n", "x" }, "ga", vim.lsp.buf.code_action, args)
   end,
 })
