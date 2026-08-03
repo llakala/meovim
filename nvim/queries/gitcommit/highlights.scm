916e1c92548df4abc6644e717a9c5f5ccaf5fc2c
Eman Resu <elevenaka11@gmail.com>
2026-08-03 00:24:25 -0400

    queries/gitcommit: stop commit messages at length 72
    There's no good reason to keep your commit messages <= 50 character.
    There _is_ a good reason to keep them <= 72 characters - if you don't,
    `git log` will put it on multiple lines!

--- nvim/queries/gitcommit/highlights.scm
+++ nvim/queries/gitcommit/highlights.scm
@@ -1,8 +1,8 @@
 ;; extends
 
 ((subject) @comment.error
-  (#vim-match? @comment.error ".\{50,}")
-  (#offset! @comment.error 0 50 0 0))
+  (#vim-match? @comment.error ".\{72,}")
+  (#offset! @comment.error 0 72 0 0))
 
 ((message_line) @comment.error
   (#vim-match? @comment.error ".\{72,}")
